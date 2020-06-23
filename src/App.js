271cdd130ebbc4c809b4c3aff31a909d43d50890
mahmoudfathy557 <mahmoudfathy557@gmail.com>
2020-06-23 06:43:09 +0200

    App comp strucutred

--- src/App.js
+++ src/App.js
@@ -5,13 +5,53 @@ import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 // showing vs-code github setu p
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: 'wake up' },
+      { id: 2, title: 'buy stuff' },
+      { id: 3, title: 'go  walking' },
+    ],
+    id: uuid(),
+    item: '',
+    editItem: false,
+  }
+
+  handleChange = (e) => {
+    console.log('handlechange')
+  }
+  handleSubmit = (e) => {
+    console.log('handleSubmit')
+  }
+  clearList = (e) => {
+    console.log('clear list')
+  }
+  handleDelete = (id) => {
+    console.log('handle delete ', id)
+  }
+  handleEdit = (id) => {
+    console.log('editing ', id)
+  }
+
   render() {
     return (
       <div>
         <div className='container'>
           <div className='row'>
-            <TodoInput />
-            <TodoList />
+            <div className='col-10 mx-auto col-md-8 mt-5 '>
+              <h3 className='text-capitalize text-center'>Todo Input</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
           </div>
         </div>
       </div>
